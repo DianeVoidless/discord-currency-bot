2fb057a15d16ede37d6b373326681e9fd080a803
DianeVoidless <neagu.diana2003@gmail.com>
2026-06-15 23:37:13 +0300

    initial bot setup

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+client.run(TOKEN)
